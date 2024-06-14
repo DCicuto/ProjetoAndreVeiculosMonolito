51d20b8b6f4897644c938181e43b759a1554f87b
Daiane Costa <cicuto.adv@gmail.com>
2024-06-14 00:49:37 -0300

    Add project files.

---
+++ ProjAPICarro/Migrations/20240614033557_3 migrao.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjAPICarro.Migrations
+{
+    public partial class _3migrao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
